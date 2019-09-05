a6b01415ab9bac0962b105fa48a94eed4d5ea1c9
sfambach <spam@bobbelsche.de>
2019-09-05 11:19:10 +0200

    First upload

---
+++ shields/L298PHighPowerMotorBridge/L298PHighPowerMotorBridgeTest/L298PHighPowerMotorBridgeTest.ino
@@ -0,0 +1,142 @@
+/** This is an example Program for the FunuMotor Shield with L298 Motor Driver
+ *  
+ *  GPL2 have fun ;) 
+ *  Used Libs:    
+ *  https://github.com/evert-arias/EasyBuzzer
+ *  
+ *  For german explanation visit 
+ *  http://www.fambach.net
+ */
+#include"src/easybuzzer/EasyBuzzer.h"
+
+#define BUZZER_PIN  4
+#define PWM1_PIN    10
+#define DIR1_PIN    12
+#define PWM2_PIN    11
+#define DIR2_PIN    13
+
+unsigned int frequency = 400;
+unsigned int duration = 1000;
+
+
+/** play a test tone */
+double testBuzzer() {
+  // EasyBuzzer.beep(500, 1); // freq 1000, 10 times.
+  EasyBuzzer.singleBeep(
+    frequency,  // Frequency in hertz(HZ).
+    duration   // Duration of the beep in milliseconds(ms).
+
+  );
+}
+
+void setup() {
+
+  // init serial
+  Serial.begin(115200);
+
+  // init motor pins
+  pinMode( PWM1_PIN, OUTPUT);
+  pinMode( DIR1_PIN, OUTPUT);
+  pinMode( PWM2_PIN, OUTPUT);
+  pinMode( DIR2_PIN, OUTPUT);
+
+  // buzzer
+  pinMode( BUZZER_PIN, OUTPUT);
+  EasyBuzzer.setPin(BUZZER_PIN);
+
+  Serial.println("Start Test");
+
+}
+
+
+
+
+void loop() {
+
+  Serial.print( "Drive in first direction\t");
+  // one direction
+  digitalWrite(DIR1_PIN, HIGH);
+  digitalWrite(DIR2_PIN, HIGH);
+  for (int i = 0; i < 255 ; i++) {
+    EasyBuzzer.update();
+    Serial.print(i % 10 == 0 ? "." : "");
+    analogWrite(PWM1_PIN, i);
+    analogWrite(PWM2_PIN, i);
+    delay(100);
+
+  }
+
+  Serial.println( " STOP");
+  // stop
+  analogWrite(PWM1_PIN, 0);
+  analogWrite(PWM2_PIN, 0);
+  delay(1000);
+
+  testBuzzer();
+
+
+  Serial.print( "Drive in second direction\t");
+  // the other direction
+  digitalWrite(DIR1_PIN, LOW);
+  digitalWrite(DIR2_PIN, LOW);
+
+  for (int i = 0; i < 255 ; i++) {
+    EasyBuzzer.update();
+    Serial.print(i % 10 == 0 ? "." : "");
+    analogWrite(PWM1_PIN, i);
+    analogWrite(PWM2_PIN, i);
+    delay(100);
+
+  }
+
+  // stop
+  Serial.println( " STOP");
+  analogWrite(PWM1_PIN, 0);
+  analogWrite(PWM2_PIN, 0);
+  delay(1000);
+  testBuzzer();
+
+  Serial.print( "Drive motor 1\t\t\t");
+  digitalWrite(DIR1_PIN, HIGH);
+  digitalWrite(DIR2_PIN, LOW);
+
+  for (int i = 0; i < 255 ; i++) {
+    EasyBuzzer.update();
+    Serial.print(i % 10 == 0 ? "." : "");
+    analogWrite(PWM1_PIN, i);
+    // analogWrite(PWM2_PIN, i);
+    delay(100);
+
+  }
+
+  // stop
+  Serial.println( " STOP");
+  analogWrite(PWM1_PIN, 0);
+  analogWrite(PWM2_PIN, 0);
+  delay(1000);
+
+  testBuzzer();
+
+
+  Serial.print( "Drive motor 2\t\t\t");
+  digitalWrite(DIR1_PIN, LOW);
+  digitalWrite(DIR2_PIN, HIGH);
+
+  for (int i = 0; i < 255 ; i++) {
+    EasyBuzzer.update();
+    Serial.print(i % 10 == 0 ? "." : "");
+    analogWrite(PWM1_PIN, i);
+    analogWrite(PWM2_PIN, i);
+    delay(100);
+
+  }
+
+  // stop
+  Serial.println( " STOP");
+  analogWrite(PWM1_PIN, 0);
+  analogWrite(PWM2_PIN, 0);
+  delay(1000);
+
+  testBuzzer();
+}
+
---
+++ shields/L298PHighPowerMotorBridge/readme.md
@@ -0,0 +1,31 @@
+Item Description:
+ 
+ 
+L298P motor driver shield adopts L298P driver chip, 
+such chip is exclusively made for high-quality large power motor. 
+It can directly drive two DC motors with driving current reaching 2A. 
+The output port of the motor carries eight high speed Schottky diodes as its protection. 
+The stackable design makes it possible to be directly connected to FOR Arduino, 
+making it more convenient to use! (Source AliExpress)
+ 
+ 
+Features:
+ 
+Input voltage (VD) of logical part: 5V
+input voltage (VS) of driving part: VIN input 6.5~12V; PWRIN input 4.8~24V
+Working current (Iss) of logical part:≤36 mA
+Working current (Io) of driving part: ≤ 2 A
+Maximum dissipation power: 25 w (T = 75 °)
+Input level of control signal: high level: 2 . 3 V≤V i n≤5 V; low level: - 0 . 3 V≤V i n≤1 . 5 V
+Working temperature: - 25 + 130 °
+Drive: Dual road large power H bridge drive
+
+
+![Image of shield](./img/L298PHighPowerMotorBridgePerspective.jpg)
+
+
+and needs the following libraries:
+
+
+
+Visit http://www.fambach.net for more informations.
