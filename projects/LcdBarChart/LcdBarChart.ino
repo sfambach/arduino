b1bf1a7154f94665b35791fd536ee4649a015661
Stefan Fambach <spam1@bobbelsche.de>
2022-02-27 23:17:49 +0100

    next

--- projects/LcdBarChart/LcdBarChart.ino
+++ projects/LcdBarChart/LcdBarChart.ino
@@ -1,23 +1,21 @@
 /**
- * Display a bar chart on an lcd displa 2 Lines 16 Cols
- * 
- * GPLv3
- * Visit https://fambach.net if you want
- */
+   Display a bar chart on an lcd displa 2 Lines 16 Cols
+
+   GPLv3
+   Visit https://fambach.net if you want
+*/
 
 
 /******************************************************************/
 // global data
-const uint8_t BAR_COUNT = 16;
+#define BAR_COUNT 16 // how many bars should we display
 int values[BAR_COUNT];
-uint8_t rows = 2;
-uint8_t columns = 16;
 
-
-// value settings
-int UPPER = 1000;
-int LOWER = 100;
-
+#define ROWS 2        // rows of display
+#define COLS 16       // columns of display
+#define UPPER 1000    // upper value of measurement eg 1065 hpa 
+#define LOWER 100     // lower value of measurement eg 650 hpa
+#define CHAR_LINES 8  // lines in a character
 
 /******************************************************************/
 // LCD init
@@ -26,94 +24,83 @@ int LOWER = 100;
 
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
-void initDisplay(){
-   lcd.begin(columns, rows);       // start the library
-   lcd.setCursor(0,0);
-   lcd.print("Bar Chart"); // print a simple message
-   initCustomChars(lcd);
-   digitalWrite(6,LOW); // only for my board to switch the buzzer off
-   delay(2000);
+void initDisplay() {
+  lcd.begin(COLS, ROWS);    // start the library
+  lcd.setCursor(0, 0);      // set the cursor to the upper left corner
+  initCustomChars(lcd);     // add the own chracters 
+    
+  lcd.print("Bar Chart");   // print a simple message
+  digitalWrite(6, LOW);     // only for my board to switch the buzzer off
+  delay(2000);              // wait some seconds to show the text
 }
 
-void displayBars(){
-    lcd.clear();
+void displayBars() {
+  lcd.clear();
+  // determine which is smaller the screen or the array, take the smaller one
+  int end =  min(COLS ,  BAR_COUNT);
 
+  // loop over the array with values 
+  for (int col = 0; col < end ; col ++) { 
 
-    if( columns >= BAR_COUNT){
+    // map the measurement values to pixel lines
+    uint8_t value = map(values[col], LOWER, UPPER, 0, 16);
 
-      // 1 bar = 1 colum, go over all columns
-      for(int col = 0; col < BAR_COUNT ; col ++){
-        
-        uint8_t value = map(values[col], LOWER,UPPER,0,16);
+    // loop over the rows of the display and set the right character
+    for (int row = ROWS - 1; row >= 0 ; row--) {
+      lcd.setCursor(col, row);
 
-        if(value > 8 ){ // upper and lower field needed
-          lcd.setCursor(col,0);
-          lcd.write((byte) value-8); 
-          
-          lcd.setCursor(col,1);
-          lcd.write((byte) 8); 
-          
-        } else if(value > 0) {
-        
-            lcd.setCursor(col,1);
-            lcd.write((byte)value); 
-        }
+      if (value == 0) {
+        break;
       }
 
-      
-    }else {
-      // not defined now
+      if ( (value - CHAR_LINES) > 0 ) {
+        lcd.write((byte) CHAR_LINES);
+      } else {
+        lcd.write((byte)value);
+        break;
+      }
+      value -= CHAR_LINES;
     }
-    digitalWrite(6,LOW); // only for my board to switch the buzzer off
-  
+
+  }
+  digitalWrite(6, LOW); // only for my board to switch the buzzer off
+
 }
 
-
 /******************************************************************/
-
-
-
-void initArray(boolean randValues){
-  for(int i =0; i < BAR_COUNT; i++){
-     if(randValues){
-      values[i] = random(LOWER, UPPER);      
-    }else {
-      values[i] = LOWER;      
+// array functions
+void initArray(boolean randValues) {
+  for (int i = 0; i < BAR_COUNT; i++) {
+    if (randValues) {
+      values[i] = random(LOWER, UPPER);
+    } else {
+      values[i] = LOWER;
     }
   }
 }
 
-void shiftArrayToTheRight(){
-  for(int i = BAR_COUNT-1 ; i > 0 ; i--){
-    values[i] = values[i-1];
+void shiftArrayToTheRight() {
+  for (int i = BAR_COUNT - 1 ; i > 0 ; i--) {
+    values[i] = values[i - 1];
   }
-
-  values[0]=0;
+  values[0] = 0;
 }
 
-
 /******************************************************************/
+void setup() {
+  initArray(false);
+  initDisplay();
+}
 
+void loop() {
 
- void setup(){
-
-    initArray(false);
-    initDisplay();
-  
- }
-
-
-
-void loop(){
-
-  //initArray(true);
   // shift array to the right
   shiftArrayToTheRight();
   // add new value at left
-  values[0] = random(LOWER, UPPER); 
+  values[0] = random(LOWER, UPPER);
 
-  
+
   displayBars();
   delay(4000);
-  
+
 }
